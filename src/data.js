064f4a390c83924c63d9279a6e90d851ebedc355
hardikd143 <hardik.d@applocum.org>
2022-12-22 17:27:16 +0530

    sidebar height update

--- src/data.js
+++ src/data.js
@@ -9,4 +9,24 @@ export const colors = [
     background: "#562B08",
     color: "#fff",
   },
+  {
+    background: "#ffffb4",
+    color: "#000",
+  },
+  {
+    background: "#F0FF42",
+    color: "#000",
+  },
+  {
+    background: "#82CD47",
+    color: "#000",
+  },
+  {
+    background: "#E5BA73",
+    color: "#000",
+  },
+  {
+    background: "#BA94D1",
+    color: "#000",
+  },
 ];
